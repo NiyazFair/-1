e0ed65466442aceb07b573e04e8d62e5f46bab16
NiyazFair <niyaz048@gmail.com>
2024-05-27 08:53:24 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+task_number = 12
+hour_number = 1.5
+name_course = 'Python'
+task_time = 1.5 / 12
+print("Курс:", name_course,', ''всего задач:', task_number,', ''затрачено часов:', hour_number,', ''среднее время выполнения ', task_time,' ' "часа.", sep = '')
